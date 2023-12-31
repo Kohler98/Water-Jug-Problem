a07755d46a9eb15ae2c94e937a6d7d9ebe860ea9
Victor Leon <victorlyonfuentes1998@gmail.com>
2023-12-31 08:00:52 -0400

    Changes in some folder's name

--- controllers/waterJugController.js
+++ controllers/waterJugController.js
@@ -1,9 +1,8 @@
 const { response } = require("express");
-const { breadthFirstSearch } = require("../helpers/waterJugSolution");
+const { breadthFirstSearch } = require("../handler/handlerWaterJugSolution");
 
 const waterJugResponse = (req,res = response) => {
 
-<<<<<<< HEAD
     try {
         
         const {bucketX,bucketY,amountWantedZ} = req.body
@@ -15,14 +14,6 @@ const waterJugResponse = (req,res = response) => {
     } catch (error) {
         res.json({msg:"missing data"})
     }
-=======
-    const {bucketX,bucketY,amountWantedZ} = req.body
-    const jug1 = parseInt(bucketX)
-    const jug2 = parseInt(bucketY)
-    const resp = breadthFirstSearch(jug1,jug2,amountWantedZ)
-
-    res.json({...resp})
->>>>>>> 25a8b70d21921562db685c4b56cdfb5efddfb73f
 }
 
 module.exports = waterJugResponse
